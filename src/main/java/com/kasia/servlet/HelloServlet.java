6467cccf5f3eb65c7ea2132bf39250eaf17efb59
unknown <k.maczkowska@gmail.com>
2017-03-07 21:04:27 +0100

    Added doPost in HelloServlet class

--- src/main/java/com/kasia/servlet/HelloServlet.java
+++ src/main/java/com/kasia/servlet/HelloServlet.java
@@ -3,11 +3,15 @@ package com.kasia.servlet;
 import com.kasia.controller.Controller;
 import com.kasia.controller.UserController;
 import org.apache.commons.lang3.StringUtils;
+import sda.file.FileOperations;
+import sda.file.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -40,5 +44,23 @@ public class HelloServlet extends HttpServlet {
 
         PrintWriter writer = resp.getWriter();
         writer.write("<h1>Hello World</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        String body = reader.readLine();
+        String[] split = body.split(" ");
+
+        User user = new User();
+        user.setFirstName(split[0]);
+        user.setLastName(split[1]);
+        user.setAge(new Integer(split[2]));
+
+        File file = new File("C:\\Users\\RENT\\Desktop\\test.txt");
+        FileOperations.addUserToFile(user, file);
+
+        resp.getWriter().write("Wszystko ok");
+        resp.setStatus(201);
     }
 }
